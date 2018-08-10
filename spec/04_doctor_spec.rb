4954907e6f7be45fe6b7474676a9fdf2c4f51bb5
Azhar Shah <azharshahstl@yahoo.com>
2018-08-10 18:33:49 +0000

    automatically backed up by learn

--- spec/04_doctor_spec.rb
+++ spec/04_doctor_spec.rb
@@ -21,8 +21,9 @@ describe "Doctor" do
     it "given a date and a patient, creates a new appointment" do
       doctor_who = Doctor.new("The Doctor")
       hevydevy = Patient.new("Devin Townsend")
+      
       appointment = doctor_who.new_appointment(hevydevy, "Friday, January 32nd")
-      binding.pry
+     
       expect(doctor_who.appointments).to include(appointment)
       expect(appointment.doctor).to eq(doctor_who)
     end

adf051c59a154e36bd01d1bb54e0589c3327ec11
Azhar Shah <azharshahstl@yahoo.com>
2018-08-10 18:03:09 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,7 +14,7 @@ class Patient
   end 
   
   def new_appointment(doctor, date) 
-    appointment = Appointment.new(date, doctor, self)
+    appointment = Appointment.new(self, doctor, date)
   end 
   
   def appointments 

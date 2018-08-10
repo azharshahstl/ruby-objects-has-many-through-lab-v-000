570d9ee35f1e8bc246b158620ffd36cfc202af82
Azhar Shah <azharshahstl@yahoo.com>
2018-08-10 16:53:11 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -2,11 +2,11 @@ class Patient
   
   attr_accessor :name
   
-  @all = [] 
+  @@all = [] 
   
   def initialize(name)
     @patient = name
-    @all << self
+    @@all << self
   end
   
   def self.all 

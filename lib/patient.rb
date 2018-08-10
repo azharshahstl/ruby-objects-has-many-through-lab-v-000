94e43279511eff66d11a161bec06bd454477ccf1
Azhar Shah <azharshahstl@yahoo.com>
2018-08-10 16:50:21 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -18,7 +18,10 @@ class Patient
   end 
   
   def appointments 
-    Appointments.all.select {|appointment| appointment.patient}
-    
+    Appointments.all.select {|appointment| appointment.patient == self}
+  end 
   
+  def doctors 
+    self.appointments.collect {|appointment| appointment.doctor}
+  end
 end
